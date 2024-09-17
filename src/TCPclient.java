defa08fa4ccd1de28f3f3959411fb591162a3866
Jamison Tucker <jamison.tucker@bsu.edu>
2024-09-16 23:50:00 -0400

    Add main to client and server

--- src/TCPclient.java
+++ src/TCPclient.java
@@ -1,2 +1,8 @@
 public class TCPclient {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Please provide <serverIP> and <serverPort>");
+            return;
+        }
+    }
 }
--- src/TCPserver.java
+++ src/TCPserver.java
@@ -1,2 +1,9 @@
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+
 public class TCPserver {
+    public static void main(String[] args) throws Exception {
+        ServerSocketChannel listenChannel = ServerSocketChannel.open();
+        listenChannel.bind(new InetSocketAddress(3000));
+    }
 }
