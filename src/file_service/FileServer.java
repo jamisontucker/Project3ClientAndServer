d3c47526fe5ab5ddc926ad747dcea628386788e9
Jamison Tucker <jamison.tucker@bsu.edu>
2024-09-18 11:36:08 -0400

    Add skeleton to server

--- src/file_service/FileServer.java
+++ src/file_service/FileServer.java
@@ -1,4 +1,38 @@
 package file_service;
 
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 public class FileServer {
+    public static void main(String[] args)throws Exception{
+        int port = 3000;
+        ServerSocketChannel listenChannel = ServerSocketChannel.open();
+        listenChannel.bind(new InetSocketAddress(port));
+        while(true){
+            SocketChannel serverChannel = listenChannel.accept();
+            ByteBuffer request = ByteBuffer.allocate(1024);
+            int numBytes = serverChannel.read(request);
+            request.flip();
+            //the size of the byte[] should match the number of bytes of the commands
+            byte[] a = new byte[3];
+            request.get(a);
+            String command = new String(a);
+            switch (command){
+                case "del":
+                    break;
+                case "lis":
+                    break;
+                case "ren":
+                    break;
+                case "upl":
+                    break;
+                case "dow":
+                    break;
+                default:
+                    System.out.print("Invalid command!");
+            }
+        }
+    }
 }
